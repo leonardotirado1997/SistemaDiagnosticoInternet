08f62cc5cd85c62844a691e5e2f4e53cf9847183
leonardotirado1997 <leopirestirado1997@gmail.com>
2026-06-01 22:02:53 -0300

    Novo arquivo de diagnostico com sintomas

---
+++ SE_diagnostico_sintomas.py
@@ -0,0 +1,23 @@
+# Dicionario - Base de Conhecimento
+
+regras = {
+    ("febre", "tosse"): "Gripe",
+    ("dor_garganta", "febre"): "Infecção na garganta",
+    ("vomito", "dor_estomago"): "Intoxicação Alimentar (virose, caganeira)"
+}
+
+#####################################
+# INTRODUÇÃO DO SISTEMA
+#####################################
+print("Bem vindos ao sistema especialista em diagnósticos médicos")
+
+#####################################
+# Inicio do programa
+#####################################
+
+# cria um array de sintomas para ser preenchido em tempo de execução 
+sintomas_agrupados = []
+
+# Pergunta os sintomas até a pessoa digitar FIM
+while True:
+    sintoma = input('Informe um sintoma ou fim \n Sintomas Disponiveis: \n("febre", "tosse", "dor_garganta", "febre", "vomito", "dor_estomago"): ')
